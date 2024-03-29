bd26e61a141a5fe700d1538011cdebf23e3cd392
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2024-03-29 12:36:30 -0500

    Refactor dropdown menu to use popover

--- src/components/ui/Auth.tsx
+++ src/components/ui/Auth.tsx
@@ -4,11 +4,10 @@ import Image from 'next/image';
 import { ChevronDown } from 'lucide-react';
 
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
 import { LinkComponent } from './LinkComponent';
 import { AuthOptions } from './AuthOptions';
 
@@ -24,8 +23,8 @@ export async function Auth() {
   }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+    <Popover>
+      <PopoverTrigger asChild>
         <div className="flex gap-3 items-center hover:cursor-pointer">
           <Image
             alt="name"
@@ -36,9 +35,9 @@ export async function Auth() {
           />
           <ChevronDown className="w-5 h-5" />
         </div>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-60">
-        <DropdownMenuLabel className="flex flex-col gap-3 items-center pt-5">
+      </PopoverTrigger>
+      <PopoverContent className="w-60">
+        <div className="flex flex-col gap-3 items-center pt-5">
           <Image
             alt="name"
             src="https://avatars.githubusercontent.com/u/97652778?v=4"
@@ -50,9 +49,9 @@ export async function Auth() {
             Yenaro Joel Noa Camino
           </span>
           <AuthOptions />
-        </DropdownMenuLabel>
+        </div>
 
-      </DropdownMenuContent>
-    </DropdownMenu>
+      </PopoverContent>
+    </Popover>
   );
 }
